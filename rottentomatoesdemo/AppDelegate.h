bcf7f25be6bd2422ce623801f1016e5751a8d63e
Hirak Chatterjee <hirakchatterjee@gmail.com>
2014-06-09 18:46:18 -0700

    Initial Commit

---
+++ rottentomatoesdemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rottentomatoesdemo
+//
+//  Created by Hirak Chatterjee on 6/9/14.
+//  Copyright (c) 2014 Hirak Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
